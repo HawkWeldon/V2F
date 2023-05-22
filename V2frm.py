82e9582493de6f738fdc1033249d81dedd7de188
Hawk <75161814+HawkWeldon@users.noreply.github.com>
2023-05-23 02:26:13 +0530

    Update V2frm.py

--- V2frm.py
+++ V2frm.py
@@ -1,12 +1,12 @@
 import cv2
 import os
 import numpy as np
-vid = cv2.VideoCapture('D:/Code2.0/V2frm/Ip/inp.mp4')
+vid = cv2.VideoCapture('')
 success,image = vid.read()
 c = 0
 while success:
   os.system('cls')
-  cv2.imwrite("D:/Code2.0/V2frm/Op/Frame%05d.png" % c, image)     # save frame as JPEG file      
+  cv2.imwrite("D:/Code2.0/V2frm/Op/Frame%05d.png" % c, image)    
   success,image = vid.read()
   print('Reading frame: ', c)
   c = c + 1
